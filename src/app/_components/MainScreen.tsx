ef327ff5e0fe1502e6388322ab778f1db9062c93
SI <>
2024-07-20 11:14:46 +0900

    feat: main画面コンポーネント作成

---
+++ src/app/_components/MainScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// 他のコンポーネントのインポートが必要です
+
+const MainScreen = () => {
+  // 状態管理のためのコードがここに来ます
+
+  return (
+    <div>
+      <h1>Americagon</h1>
+      {/* ここに他のコンポーネントを配置します */}
+      {/* <GameBoard />
+      <ScoreBoard />
+      <Timer /> */}
+      {/* 他の必要なコンポーネント */}
+    </div>
+  );
+};
+
+export default MainScreen;
--- src/app/page.client.tsx
+++ src/app/page.client.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React from 'react';
+import MainScreen from '@/app/_components/MainScreen'
 
 const Pageclient = () => {
   return (
     <div>
-      <p>This is a page client</p>
+      <MainScreen />
     </div>
   );
 };
