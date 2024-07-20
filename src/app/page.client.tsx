6b5caa04558935a1fc073fbd169c7c26b91c4be6
SI <>
2024-07-20 10:59:22 +0900

    feat: add top page

---
+++ src/app/page.client.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const Pageclient = () => {
+  return (
+    <div>
+      <p>This is a page client</p>
+    </div>
+  );
+};
+
+export default Pageclient;
